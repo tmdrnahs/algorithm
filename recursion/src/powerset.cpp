86871b099d7d7dba30ba131475593c4f70ad023c
tmdrnahs <37217634+tmdrnahs@users.noreply.github.com>
2019-08-01 18:21:15 +0900

    recursion: powerset done

--- recursion/src/powerset.cpp
+++ recursion/src/powerset.cpp
@@ -2,34 +2,26 @@
 
 using namespace std;
 
-#define SIZE 7
-char array[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-bool mark[] = {false,};
+#define SIZE 4
+char array[] = {'a', 'b', 'c', 'd'};
+bool mark[SIZE];
 
 void PowerSet(int k)
 {
-    if (k == SIZE)
-    {
-        for (int i = 0; i < SIZE; i++)
-            cout << array[i];
-    }
-    else
-    {
-        /*
-            Under Implementing
-        */
-        for (int i = k; i < SIZE; i++)
-        {
-            if (mark[k])
-                cout << array[k];
+    if (k == SIZE) {
+        for (int i = 0; i < SIZE; i++) {
+            if (mark[i])
+                cout << array[i];
         }
         cout << endl;
-
-        mark[k] = false;
-        PowerSet(k+1);
-        mark[k] = true;
-        PowerSet(k+1);
+        return;
     }
+
+    mark[k] = false;
+    PowerSet(k+1);
+
+    mark[k] = true;
+    PowerSet(k+1);
 }
 
 int main(void)
